9b4184a875e3ec9c70b539507a0e5588585dfc91
MateoNerli <mnerli2003@gmail.com>
2023-10-21 14:47:33 -0300

    pagina de contacto

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,13 @@
 //header
-header = `<h1 class="text-5xl text-center my-14">
-Renov <span class="text-[#8d4925]">Arte</span>
-</h1>`;
+header = `<div class="flex items-center justify-center">
+<h1 class="text-5xl text-center my-14">
+  Renov <span class="text-[#8d4925]">Arte</span>
+</h1>
+
+</div>
+
+
+`;
 document.getElementById("header").innerHTML = header;
 
 //MENU
@@ -22,115 +28,63 @@ nav = `
   <a class="items-center block p-4 text-2xl hover:border-b-2" href="#sobre-nosotros">Nosotros</a>
   <a class="items-center block p-4 text-2xl hover:border-b-2" href="#productos">Algunas restauraciones</a>
   <a class="items-center block p-4 text-2xl hover:border-b-2" href="./galeria.html">Galeria</a>
-  <a class="items-center block p-4 text-2xl hover:border-b-2" href="contacto.html">Contacto</a>
+  <a class="items-center block p-4 text-2xl hover:border-b-2" href="./contacto.html">Contacto</a>
   <a class="items-center block p-4 text-2xl hover:border-b-2" href="login.html">Login</a>
 </nav>`;
 document.getElementById("navbar").innerHTML = nav;
 
 //footer
-footer = `<div class="container mx-auto flex flex-wrap justify-between">
-<div class="w-full md:w-1/4">
-  <h3 class="text-lg font-semibold mb-4">Categorías</h3>
-  <nav class="space-y-2 flex flex-col">
-    <a href="#" class="text-gray-400 hover:text-white">Cocina</a>
-    <a href="#" class="text-gray-400 hover:text-white">Oficina</a>
-    <a href="#" class="text-gray-400 hover:text-white">Jardín</a>
-    <a href="#" class="text-gray-400 hover:text-white">Cochera</a>
-    <a href="#" class="text-gray-400 hover:text-white">Dormitorios</a>
-  </nav>
+footer = `
+<div class="container mx-auto flex flex-wrap justify-between">
+  <div class="w-full md:w-1/4">
+    <h3 class="text-lg font-semibold mb-4">Categorías</h3>
+    <nav class="space-y-2 flex flex-col">
+      <a href="#" class="text-gray-400 hover:text-white">Cocina</a>
+      <a href="#" class="text-gray-400 hover:text-white">Oficina</a>
+      <a href="#" class="text-gray-400 hover:text-white">Jardín</a>
+      <a href="#" class="text-gray-400 hover:text-white">Cochera</a>
+      <a href="#" class="text-gray-400 hover:text-white">Dormitorios</a>
+    </nav>
+  </div>
+
+  <div class="w-full md:w-1/4 flex flex-col">
+    <h3 class="text-lg font-semibold mb-4">Sobre Nosotros</h3>
+    <nav class="space-y-2">
+      <a href="#" class="text-gray-400 hover:text-white">Nuestra Historia</a>
+      <a href="#" class="text-gray-400 hover:text-white">Misión, Visión y Valores</a>
+      <a href="#" class="text-gray-400 hover:text-white">Carreras</a>
+      <a href="#" class="text-gray-400 hover:text-white">Políticas de Privacidad</a>
+      <a href="#" class="text-gray-400 hover:text-white">Términos del Servicio</a>
+    </nav>
+  </div>
+
+  <div class="w-full md:w-1/4 flex flex-col">
+    <h3 class="text-lg font-semibold mb-4">Soporte</h3>
+    <nav class="space-y-2">
+      <a href="#" class="text-gray-400 hover:text-white">Preguntas Frecuentes</a>
+      <a href="#" class="text-gray-400 hover:text-white">Ayuda en línea</a>
+      <a href="#" class="text-gray-400 hover:text-white">Confianza y Seguridad</a>
+    </nav>
+  </div>
 </div>
 
-<div class="w-full md:w-1/4 flex flex-col">
-  <h3 class="text-lg font-semibold mb-4">Sobre Nosotros</h3>
-  <nav class="space-y-2">
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Nuestra Historia</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Misión, Visión y Valores</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white">Carreras</a>
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Políticas de Privacidad</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Términos del Servicio</a
-    >
-  </nav>
-</div>
-
-<div class="w-full md:w-1/4 flex flex-col">
-  <h3 class="text-lg font-semibold mb-4">Soporte</h3>
-  <nav class="space-y-2">
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Preguntas Frecuentes</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Ayuda en línea</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Confianza y Seguridad</a
-    >
-  </nav>
-</div>
-</div>
-
-<p class="text-center mt-8 text-gray-400">
-Todos los Derechos Reservados - RenovArte
-</p>`;
-document.getElementById("footer").innerHTML = footer;
-
-//footer
-footer = `<div class="container mx-auto flex flex-wrap justify-between">
-<div class="w-full md:w-1/4">
-  <h3 class="text-lg font-semibold mb-4">Categorías</h3>
-  <nav class="space-y-2 flex flex-col">
-    <a href="#" class="text-gray-400 hover:text-white">Cocina</a>
-    <a href="#" class="text-gray-400 hover:text-white">Oficina</a>
-    <a href="#" class="text-gray-400 hover:text-white">Jardín</a>
-    <a href="#" class="text-gray-400 hover:text-white">Cochera</a>
-    <a href="#" class="text-gray-400 hover:text-white">Dormitorios</a>
-  </nav>
-</div>
-
-<div class="w-full md:w-1/4 flex flex-col">
-  <h3 class="text-lg font-semibold mb-4">Sobre Nosotros</h3>
-  <nav class="space-y-2">
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Nuestra Historia</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Misión, Visión y Valores</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white">Carreras</a>
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Políticas de Privacidad</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Términos del Servicio</a
-    >
-  </nav>
-</div>
-
-<div class="w-full md:w-1/4 flex flex-col">
-  <h3 class="text-lg font-semibold mb-4">Soporte</h3>
-  <nav class="space-y-2">
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Preguntas Frecuentes</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Ayuda en línea</a
-    >
-    <a href="#" class="text-gray-400 hover:text-white"
-      >Confianza y Seguridad</a
-    >
-  </nav>
-</div>
+<!-- Social Media Icons -->
+<div class="flex justify-center space-x-4 mt-8">
+  <a href="https://www.instagram.com/renovarte.sn/?theme=dark" class="text-gray-400 hover:text-white">
+    <i class="fab fa-instagram"></i>
+  </a>
+  <a href="https://www.facebook.com/profile.php?id=100093497345781" class="text-gray-400 hover:text-white">
+    <i class="fab fa-facebook"></i> 
+  </a>
+  <a href="#" class="text-gray-400 hover:text-white">
+    <i class="fab fa-whatsapp"></i>
+  </a>
 </div>
 
 <p class="text-center mt-8 text-gray-400">
-Todos los Derechos Reservados - RenovArte
-</p>`;
+  Todos los Derechos Reservados - RenovArte
+</p>
+`;
 
 //menu
 document.getElementById("footer").innerHTML = footer;
